1a0acd4f3fc352dbdc0779db389354dcdb323521
Pedro Cechetto <pedrocechetto@hotmail.com>
2023-06-24 18:39:20 -0300

    ლ(ಠ益ಠლ)

--- src/controllers/orders.controller.ts
+++ src/controllers/orders.controller.ts
@@ -1,8 +1,8 @@
 import { Controller, Get, Post, Body } from '@nestjs/common';
 import { Order } from '@prisma/client';
-import { PrismaClient } from '@prisma/client';
+//import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
+//const prisma = new PrismaClient();
 
 @Controller('api/orders')
 export class OrdersController {
